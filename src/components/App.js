7ab78b86ff2a72e029059a7d11e237528e25aa49
joshuapawlik <joshawesome12@yahoo.com>
2018-07-31 22:48:26 -0500

    Changes Default Text

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to my boilerplate</h1>
+        <h1>Welcome to MY react boilerplate</h1>
       </div>
     )
   }
